8d61fa54290f337d32fb8fd8bd4b85b038da01fe
Amisha011 <amishaagrawal011@gmail.com>
2021-08-05 16:44:17 +0530

    third commit by amisha

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require (`express`);
+const cors = require (`cors`);
+const bodyParser= require (`body-parser`);
+const mongoose = require (`mongoose`);
+
+//created server
+const app = express();
+console.log("hello world");
+// use middlewares 
+
+app.unsubscribe(cors());
+app.use(bodyParser.json());
+
+//
+const userRouter = require("./router/user.route");
+const itemRouter = require("./router/item.route");
+
+//mongodb url 
+
+const dbURL = "mongodb+srv://Amisha:amisha011@cluster0.dujaf.mongodb.net/ResturantManagement?retryWrites=true&w=majority";
+//connect with mongoose
+mongoose.connect(dbURL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+    .then(() => {
+        console.log("Connected to MongoDb");
+
+
+        app.listen(8001, () => {
+            console.log("server is on port 8001")
+        })
+
+    })
+    .catch(error => {
+        console.log("error in coonecting with db", error)
+    })
+
+    app.use("/api/user",userRouter);
+    app.use("/api/item",itemRouter);
